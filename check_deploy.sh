7a32aa8be4988ab8c09e7daeadfdd99323d4b060
Yung-En Chi <s3864916@student.rmit.edu.au>
2023-05-27 14:14:55 +1000

    github actions

--- check_deploy.sh
+++ check_deploy.sh
@@ -6,11 +6,11 @@ url=$(cat a2_front)
 # Make the curl request and store the response in a variable
 response=$(curl -s "$url")
 
-# Check if the response contains the text "Foo app" and output true/false
+# Check if the response contains the text "Foo app" and return true/false
 if echo "$response" | grep -q "Foo app"; then
-  echo "The app is already deployed"
-  echo "Exiting the program...(with error code)"
-  exit 1 
+  echo "true"
+  exit 0
 else
   echo "false"
+  exit 0
 fi
