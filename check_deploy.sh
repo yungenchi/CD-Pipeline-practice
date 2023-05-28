1f5a7ed5361b5860dbbd42385aeb6574fc142a6c
Yung-En Chi <s3864916@student.rmit.edu.au>
2023-05-28 10:45:00 +1000

    Yung-En Chi (S3864916)

--- check_deploy.sh
+++ check_deploy.sh
@@ -9,9 +9,9 @@ response_db=$(curl -s "$url"/foos)
 
 # Check if the response contains both "Foo app" and "Big Big Foo" and return true/false
 if echo "$response_app" | grep -q "Foo app" && echo "$response_db" | grep -q "Big Big Foo"; then
-  echo "true"
+  echo "Already deployed"
   exit 0
 else
-  echo "false"
+  echo "Not yet deployed"
   exit 1
 fi
